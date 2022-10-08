017a42fe9496bf79d4a9be09bca1715c8edaa99c
dfmoura <diogomou@gmail.com>
2022-10-08 16:09:55 -0300

    1038 ok

--- 1038.cpp
+++ 1038.cpp
@@ -8,27 +8,21 @@ using namespace std;
 int main(){
 
     int opcao, quantidade;
-    double resultado;
-    string foraMenu;
-    cout << "Informar item do menu e quantidade "<< endl;
-    cout << "1 - Cahorro Quente"<< endl;
-    cout << "2 - X-Salada"<< endl;
-    cout << "3 - X-Bacon"<< endl;
-    cout << "4 - Torrada Simples"<< endl;
-    cout << "5 - Refrigerante"<< endl;
+    float resultado;
+
     cin >> opcao; 
     cin>> quantidade;
 
     if(opcao == 1){
         resultado = 4.00 * quantidade;
     }else if(opcao ==2){
-        resultado = 4.5 * quantidade;
+        resultado = 4.50 * quantidade;
     }else if(opcao ==3){
-        resultado = 5.0 * quantidade;
+        resultado = 5.00 * quantidade;
     }else if(opcao ==4){
-        resultado = 2.0 * quantidade;
+        resultado = 2.00 * quantidade;
     }else if(opcao ==5){
-        resultado = 1.5 * quantidade;
+        resultado = 1.50 * quantidade;
     }
     cout << "Total: R$ " << fixed << setprecision(2)<< resultado<< endl;
 }
