e4e3f9fdadb4ec596253da5664931e1d88eb95d3
dfmoura <diogomou@gmail.com>
2022-10-21 14:27:36 -0300

    beecrowd 1064

---
+++ UberHub/beecrawd/1564.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <math.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+
+
+using namespace std;
+int main(){
+
+    int N;
+
+    //if ((N>=0)&&(N<=100)){
+
+        while (cin >> N)
+        {
+            if(N==0){cout << "vai ter copa!"<< endl;}
+            else{cout << "vai ter duas!"<< endl;}
+        }
+        
+
+
+    //}
+
+
+}
+
+
