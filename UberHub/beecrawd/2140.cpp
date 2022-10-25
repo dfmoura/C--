88df9392a7c8ce042a347fae6fe15b85b3519df0
dfmoura <diogomou@gmail.com>
2022-10-25 00:04:21 -0300

    2140

--- UberHub/beecrawd/2140.cpp
+++ UberHub/beecrawd/2140.cpp
@@ -9,26 +9,26 @@
 using namespace std;
 int main(){
 
-        while(1)
-        {
-            int N, M, troco;
-            cin >> N >> M;
-            troco = M - N;
-            int notas[]={2,5,10,20,50,10};
-            bool possivel = false;
+    while(1)
+    {
+        int N, M, troco;
+        cin >> N >> M;
+        troco = M - N;
+        int notas[]={2,5,10,20,50,10};
+        bool possivel = false;
 
-            if((N==0) && (M==0)){break;}
-            
-            if ((N>M) || (M>pow(10,4))){break;}
+        if((N==0) && (M==0)){break;}
+        
+        if ((N>M) || (M>pow(10,4))){break;}
 
-            for (int i = 0; i < 6; i++){
-                for (int j=0; j < 6; j++){
-                    if(troco - notas[j] == notas[i]) {possivel = true;}
-                }
-            } 
+        for (int i = 0; i < 6; i++){
+            for (int j=0; j < 6; j++){
+                if(troco - notas[j] == notas[i]) {possivel = true;}
+            }
+        } 
 
-            if (possivel){cout<<"possible"<<endl;}
-            else{cout<<"impossible"<<endl;}
-        }
+        if (possivel){cout<<"possible"<<endl;}
+        else{cout<<"impossible"<<endl;}
+    }
     
 }
