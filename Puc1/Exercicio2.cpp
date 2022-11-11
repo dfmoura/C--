e2b81029516304e4bcf828ce6d4c3650b303a12f
Diogo Moura <diogomou@gmail.com>
2022-11-10 23:20:49 -0300

    Teste

--- Puc1/Exercicio2.cpp
+++ Puc1/Exercicio2.cpp
@@ -14,13 +14,17 @@ using namespace std;
 int main(){
 
     int c, d, e;
-    float a;
+    float a,b;
     cin>>c>>d>>e;
     int arr[] = { c, d, e};
     a = arr[0]+arr[1]+arr[2];
+    b = a/3;
 
         cout<<"Total dos gastos: " << setprecision(2) << fixed<<a<<endl;
-        cout<<"Valor individual: " << setprecision(2) << fixed<<a/3<<endl;       
+        cout<<"Valor individual: " << setprecision(2) << fixed<<b<<endl;       
+        
+        cout<<c-b<<" "<<d-b<<" "<<e-b<<endl;
+        
         cout<<"Porthos deve R$ 100.00 a Aramis"<<endl;
         cout<<"Athos deve R$ 100.00 a Aramis"<<endl;
         cout<<"Athos deve R$ 100.00 a Aramis e R$ 0.00 a Porthos"<<endl;
